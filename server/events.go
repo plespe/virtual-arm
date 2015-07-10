7480e8abdef418e9566cb0a310b4b11c7f4f2407
andrew li <andrew-li@users.noreply.github.com>
2015-07-10 16:42:16 -0700

    Added event names in the outgoing messages.

--- server/events.go
+++ server/events.go
@@ -2,18 +2,18 @@ package main
 
 import (
     "encoding/json"
-    "fmt"    
+    //"fmt"    
 )
 
 func handleEvent(eventName string, messageContents string, player *Player) {
   switch {
-    case eventName == "updateBodyPosition":
+    case eventName == "ubp": //updateBodyPosition
       evt_updatePlayerBodyPosition(messageContents, player)
       break
-    case eventName == "updateHeadPosition":
+    case eventName == "uhp": //updateHeadPosition
       evt_updatePlayerHeadPosition(messageContents, player)
       break
-    case eventName == "recvMessage":
+    case eventName == "sm": //sendMessage
       evt_sendMessage(messageContents, player)
       break  
     default:
@@ -22,7 +22,7 @@ func handleEvent(eventName string, messageContents string, player *Player) {
 }
 
 
-//updateBodyPosition:{"x": 1.0, "y": 2.0, "z": 3.0, "r": 4.0}
+//ubp:{"x": 1.0, "y": 2.0, "z": 3.0, "r": 4.0}
 func evt_updatePlayerBodyPosition(message string, player *Player) {
   byt := []byte(message)
   var dat map[string]interface{}
@@ -34,9 +34,10 @@ func evt_updatePlayerBodyPosition(message string, player *Player) {
   z := float32(dat["z"].(float64))
   r := float32(dat["r"].(float64)) 
   player.updateBodyPosition(x, y, z, r)
-  player.room.broadcast <- []byte(message)
+  player.room.broadcast <- []byte("ubp:" + message)
 }
 
+//uhp:{"x": 1.0, "y": 2.0, "z": 3.0, "w": 4.0}
 func evt_updatePlayerHeadPosition(message string, player *Player) {
   byt := []byte(message)
   var dat map[string]interface{}
@@ -48,9 +49,10 @@ func evt_updatePlayerHeadPosition(message string, player *Player) {
   z := float32(dat["z"].(float64))
   w := float32(dat["w"].(float64))    
   player.updateHeadPosition(x, y, z, w)    
-  player.room.broadcast <- []byte(message)
+  player.room.broadcast <- []byte("uhp:" + message)
 }
 
+//sm:test
 func evt_sendMessage(message string, player *Player) {
-  player.room.broadcast <- []byte(message)
+  player.room.broadcast <- []byte("sm:" + message)
 }
