b8c3312dd2900d684068e2e6832922d779f72342
andrew li <andrew-li@users.noreply.github.com>
2015-07-13 19:12:49 -0700

    put authentication and signup logic into server and made test clients to login and signup

--- server/auth.go
+++ server/auth.go
@@ -1,35 +1,30 @@
 package main
 
 import (
-  "crypto/md5"
+  //"crypto/md5"
   "fmt"
-  "io"
+  //"io"
   "log"
   "net/http"
-  "strings"
+  //"strings"
+  "encoding/json"
+  "io/ioutil"
   "database/sql"
   _ "github.com/go-sql-driver/mysql"
 
-  "github.com/stretchr/gomniauth"
-  "github.com/stretchr/objx"
+  //"github.com/stretchr/gomniauth"
+  //"github.com/stretchr/objx"
+  //gomniauthcommon "github.com/stretchr/gomniauth/common"
 )
-
-import gomniauthcommon "github.com/stretchr/gomniauth/common"
 
 type authHandler struct {
   next http.Handler
 }
 
-db, err := sql.Open("mysql",
-  // db address goes here:
-"user:password@tcp(127.0.0.1:3306)/hello")
-if err != nil {
-  log.Fatal(err)
-}
-defer db.Close()
-
 func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.value == "" {
+  //if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.value == "" {
+  if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie {
+    fmt.Println(cookie)
     w.Header()["Location"] = []string{"/login"}
     w.WriteHeader(http.StatusTemporaryRedirect)
   } else if err != nil {
@@ -43,52 +38,140 @@ func MustAuth(handler http.Handler) http.Handler {
   return &authHandler{next: handler}
 }
 
-func loginHandler (w http.ResponseWriter, r *http.Request) {
+func createUserHandler (w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        panic(err)
+    }
+    //fmt.Println(string(body))
+
+    byt := body
+    var dat map[string]interface{}
+    if err := json.Unmarshal(byt, &dat); err != nil {
+      panic(err)
+    }
+
+    fmt.Println(dat["username"])
+    fmt.Println(dat["password"])
+    fmt.Println(dat["firstname"])
+    fmt.Println(dat["lastname"])    
+
+    // if any of the above fields are blank
+    // return error saying so
+    for dat := range {
+      if dat == nil {
+        w.Write([]byte(fmt.Sprintf("Enter all information", dat)))
+        w.WriteHeader(http.StatusBadRequest) // or some other status?
+      }
+    }
 
     var (
-      id int
-      password_hash string
+      queried_username string
     )
 
-    row, err := db.QueryRow("select id, password_hash from users where user_name = ?", username)
+    // query the database for the username
+    err = db.QueryRow("select user_name from users where user_name = ?", username).Scan(&queried_username)
+    switch {
+    case err == sql.ErrNoRows:
+      // if username doesn't exist
+        // hash password
+        // add username, password, first and last name to database
+        // create session
+        // redirect to connect (connect will open web socket)
+    case err != nil:
+            log.Fatal(err)
+    default:
+      // if username already exists, return error saying so
+    } 
+
+}
+
+func loginHandler (w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+    fmt.Println("Authenticating user...")
+
+    body, err := ioutil.ReadAll(r.Body)
     if err != nil {
-      log.Fatal(err)
+        panic(err)
     }
-    defer row.Close()
+    //fmt.Println(string(body))
 
-   // not sure what happens when row is empty - still need to handle case
-
-    err := row.Scan(&id, &password_hash)
-    if err != nil {
-      log.Fatal(err)
-    }
-    // check password here and insert different routes
-    log.Println(id, password)
-
-    err = row.Err()
-    if err != nil {
-      log.Fatal(err)
+    byt := body
+    var dat map[string]interface{}
+    if err := json.Unmarshal(byt, &dat); err != nil {
+      panic(err)
     }
 
-    // save some data
-    authCookieValue := objx.New(map[string]interface{}{
-      "userid":     chatUser.uniqueID,
-      "name":       user.Name(),
-    }).MustBase64()
+    fmt.Println(dat["username"])
+    fmt.Println(dat["password"])
 
-    http.SetCookie(w, &http.Cookie{
-      Name:  "auth",
-      Value: authCookieValue,
-      Path:  "/"})
+    username := dat["username"]
+    //password := dat["password"]
 
-    w.Header()["Location"] = []string{"/chat"}
-    w.WriteHeader(http.StatusTemporaryRedirect)
-    break
+
+   var (
+      queried_id int
+      queried_password_hash string
+    )
+
+    err = db.QueryRow("select id, password_hash from users where user_name = ?", username).Scan(&queried_id, &queried_password_hash)
+    switch {
+    case err == sql.ErrNoRows:
+            fmt.Printf("No user with that username.")
+
+    case err != nil:
+            log.Fatal(err)
+    default:
+            fmt.Printf("Id is %d\n", queried_id)
+            fmt.Printf("Password is %s\n", queried_password_hash)
+    }
+
+
+   //  var (
+   //    id int
+   //    password_hash string
+   //  )
+
+   //  row, err := db.QueryRow("select id, password_hash from users where user_name = ?", username)
+   //  if err != nil {
+   //    log.Fatal(err)
+   //  }
+   //  defer row.Close()
+
+   // // not sure what happens when row is empty - still need to handle case
+
+   //  err := row.Scan(&id, &password_hash)
+   //  if err != nil {
+   //    log.Fatal(err)
+   //  }
+   //  // check password here and insert different routes
+   //  log.Println(id, password)
+
+   //  err = row.Err()
+   //  if err != nil {
+   //    log.Fatal(err)
+   //  }
+
+   //  // save some data
+   //  authCookieValue := objx.New(map[string]interface{}{
+   //    "userid":     chatUser.uniqueID,
+   //    "name":       user.Name(),
+   //  }).MustBase64()
+
+   //  http.SetCookie(w, &http.Cookie{
+   //    Name:  "auth",
+   //    Value: authCookieValue,
+   //    Path:  "/"})
+
+   //  w.Header()["Location"] = []string{"/chat"}
+   //  w.WriteHeader(http.StatusTemporaryRedirect)
+   //  break
 
   // default:
   //   w.Write([]byte(fmt.Sprintf("Auth action %s not supported", action)))
   //   w.WriteHeader(http.StatusNotFound)
   //   break
-  }
+  // }
 }
 
--- server/server.go
+++ server/server.go
@@ -10,19 +10,43 @@ import (
     //"go/build"
     //"path/filepath"
     "text/template"   
+    "database/sql"
+    _ "github.com/go-sql-driver/mysql"    
+)
+
+const (
+    DB_USER = "root"
+    DB_PASSWORD = ""
+    DB_NAME = "virtual_arm"
 )
 
 var (
     addr      = flag.String("addr", ":8080", "http service address")
     homeTempl *template.Template
+    signupTempl *template.Template
 )
+
+func initializeDB() *sql.DB {
+        db, err := sql.Open("mysql",  DB_USER + ":" + DB_PASSWORD + "@/" + DB_NAME)
+        if err != nil {
+          panic(err)
+        } 
+
+        return db
+}
 
 func homeHandler(c http.ResponseWriter, req *http.Request) {
     homeTempl.Execute(c, req.Host)
 }
+func signupHandler(c http.ResponseWriter, req *http.Request) {
+    signupTempl.Execute(c, req.Host)
+}
 
 func main() {
     flag.Parse()
+
+    var db = initializeDB()
+    defer db.Close()    
 
     //create the game room
     var room = createGameRoom()
@@ -31,6 +55,21 @@ func main() {
     //serve the home page
     homeTempl = template.Must(template.ParseFiles("home.html"))
     http.HandleFunc("/", homeHandler)
+
+    //serve the signup page
+    signupTempl = template.Must(template.ParseFiles("signup.html"))
+    http.HandleFunc("/signup", signupHandler)
+    http.HandleFunc("/signup.html", signupHandler)
+
+    //allow user to sign up
+    http.HandleFunc("/createUser", func(w http.ResponseWriter, r *http.Request) {
+        createUserHandler(w, r, db)
+    })    
+
+    //authenticate user
+    http.HandleFunc("/authenticate", func(w http.ResponseWriter, r *http.Request) {
+        loginHandler(w, r, db)
+    })    
 
     //listen for player connection
     http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
