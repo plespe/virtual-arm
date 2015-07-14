9f233b235e26c359b8b4f7347f15fed16975d6d7
mikemsrk <min2033@gmail.com>
2015-07-14 08:27:17 -0700

    Simple routing react.

--- public/src/app.js
+++ public/src/app.js
@@ -1,4 +1,31 @@
-React.render(
-  <Login />,
-  document.getElementById('content')
+// var ReactRouter = window.ReactRouter;
+var Router = window.ReactRouter;
+var Route = ReactRouter.Route;
+var RouteHandler = Router.RouteHandler;
+var Link = ReactRouter.Link;
+
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>App</h1>
+        <RouteHandler/>
+      </div>
+      );
+  }
+});
+
+var routes = (
+  <Route handler={App}>
+    <Route path="login" handler={Login}/>
+  </Route>
 );
+
+Router.run(routes, Router.HashLocation, function(Root){
+  React.render(<Root/>, document.getElementById('content'));
+});
+
+// React.render(
+//   <Login />,
+//   document.getElementById('content')
+// );
