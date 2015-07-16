3b991353047c2e942c93154d62a6104af4fa30a3
andrew li <andrew-li@users.noreply.github.com>
2015-07-16 11:28:46 -0700

    added server ability to broadcast to specific targets, added create player and new player events, and added updated event messages

--- server/player.go
+++ server/player.go
@@ -7,38 +7,39 @@ import (
     "strings"
     "log"
     "strconv"
+    //"encoding/json"
     //"time"
 )
 
 type BodyPosition struct {
-    x float32
-    y float32
-    z float32
-    r float32
+    X float32 `json:"x"`
+    Y float32 `json:"y"`
+    Z float32 `json:"z"`
+    R float32 `json:"r"`
 }
 
 type HeadPosition struct {
-    x float32
-    y float32
-    z float32 
-    w float32      
+    X float32 `json:"x"`
+    Y float32 `json:"y"`
+    Z float32 `json:"z"`
+    W float32 `json:"w"`     
 }
 
 type Player struct {
-    id int
-    username string
+    Id int `json:"id"`
+    Username string `json:"username"`
 
-    bodyPosition *BodyPosition
-    headPosition *HeadPosition
+    BodyPosition *BodyPosition `json:"bodyPosition"`
+    HeadPosition *HeadPosition `json:"headPosition"`
 
     // The websocket connection.
-    ws *websocket.Conn
+    Ws *websocket.Conn `json:"ws"`
 
     // Buffered channel of outbound messages.
-    send chan []byte
+    Send chan []byte `json:"send"`
 
     // The hub.
-    room *GameRoom
+    Room *GameRoom `json:"room"`
 }
 
 func parseMessage(message string, player *Player) {
@@ -50,61 +51,112 @@ func parseMessage(message string, player *Player) {
 
 func (player *Player) reader() {
     for {
-        _, message, err := player.ws.ReadMessage()
+        _, message, err := player.Ws.ReadMessage()
         if err != nil {
             break
         }
-        fmt.Println(string(message))
+        //fmt.Println(string(message))
         //time.Sleep(5000 * time.Millisecond)
         parseMessage(string(message), player)
     }
-    player.ws.Close()
+    player.Ws.Close()
 }
 
 func (player *Player) writer() {
-    for message := range player.send {
-        err := player.ws.WriteMessage(websocket.TextMessage, message)
+    for message := range player.Send {
+        err := player.Ws.WriteMessage(websocket.TextMessage, message)
         if err != nil {
             break
         }
     }
-    player.ws.Close()
+    player.Ws.Close()
 }
 
 func (player *Player) updateBodyPosition(x float32, y float32, z float32, r float32) {
-  player.bodyPosition.x = x
-  player.bodyPosition.y = y
-  player.bodyPosition.z = z
-  player.bodyPosition.r = r
+  player.BodyPosition.X = x
+  player.BodyPosition.Y = y
+  player.BodyPosition.Z = z
+  player.BodyPosition.R = r
 }
 
 func (player *Player) updateHeadPosition(x float32, y float32, z float32, w float32) {
-  player.headPosition.x = x
-  player.headPosition.y = y 
-  player.headPosition.z = z  
-  player.headPosition.w = w  
+  player.HeadPosition.X = x
+  player.HeadPosition.Y = y 
+  player.HeadPosition.Z = z  
+  player.HeadPosition.W = w  
 }
 
 var upgrader = &websocket.Upgrader{ReadBufferSize: 1024, WriteBufferSize: 1024}
 
 type PlayerHandler struct {
-    id int
-    username string
-    room *GameRoom
+    Id int `json:"id"`
+    Username string `json:"username"`
+    Room *GameRoom `json:"room"`
 }
+
+
+type PlayerPositionListOutbound struct {
+    Players []*PlayerPositionOutbound `json:"players"`
+}
+
+type PlayerPositionOutbound struct {
+    Id int `json:"id"`
+    Username string `json:"username"`
+
+    BodyPosition *BodyPosition `json:"bodyPosition"`
+    HeadPosition *HeadPosition `json:"headPosition"`
+}
+
+type PlayerBodyPositionOutbound struct {
+    Id int `json:"id"`
+    Username string `json:"username"`
+
+    BodyPosition *BodyPosition `json:"bodyPosition"`
+}
+
+type PlayerHeadPositionOutbound struct {
+    Id int `json:"id"`
+    Username string `json:"username"`
+
+    HeadPosition *HeadPosition `json:"headPosition"`
+}
+
 
 func (playerHandler PlayerHandler) createPlayer(w http.ResponseWriter, r *http.Request) {
     ws, err := upgrader.Upgrade(w, r, nil)
     if err != nil {
       log.Fatal(err)
     }
-    player := &Player{send: make(chan []byte, 256), bodyPosition: &BodyPosition{x: 0.0, y: 0.0, z: 0.0, r: 0.0}, headPosition: &HeadPosition{x: 0.0, y: 0.0, z: 0.0, w: 0.0}, 
-      ws: ws, room: playerHandler.room, username: playerHandler.username, id: playerHandler.id}
-    player.room.register <- player
+    player := &Player{Send: make(chan []byte, 256), BodyPosition: &BodyPosition{X: 0.0, Y: 0.0, Z: 0.0, R: 0.0}, HeadPosition: &HeadPosition{X: 0.0, Y: 0.0, Z: 0.0, W: 0.0}, 
+      Ws: ws, Room: playerHandler.Room, Username: playerHandler.Username, Id: playerHandler.Id}
+    player.Room.Register <- player
 
-    fmt.Println("Created player " + strconv.Itoa(player.id) + " in room " + strconv.Itoa(player.room.id))
+    fmt.Println("Created player " + strconv.Itoa(player.Id) + " in room " + strconv.Itoa(player.Room.Id))
 
-    defer func() { player.room.unregister <- player }()
+
+
+    // m := make(map[int]int)
+    // m[player.id] = player.id
+
+    // //send coordinates of other players to the new player
+    // jsonMessage := "["
+    // for p := range player.room.players {
+    //     if p.id != player.id{
+    //       jsonMessage += "{\"name\": "
+    //       jsonMessage += "}"
+    //       jsonMessage += "}"
+    //     }
+    // }  
+    // jsonMessage += "]"
+
+
+    // broadcastStruct := BroadcastStruct{broadcastType: 0, targetIds: m, message: []byte("uhp:" + message)}
+    // player.room.broadcast <- &broadcastStruct
+    // //send coordinates of the new player to other players
+    // broadcastStruct = BroadcastStruct{broadcastType: 2, targetIds: m, message: []byte("uhp:" + message)}
+    // player.room.broadcast <- &broadcastStruct  
+
+    defer func() { player.Room.Unregister <- player }()
     go player.writer()
     player.reader()
 }
