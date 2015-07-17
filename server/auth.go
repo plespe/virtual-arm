1c8e5d7f2f115b21fadee327b8f8ce462d86c07a
andrew li <andrew-li@users.noreply.github.com>
2015-07-17 10:40:37 -0700

    Refactored server code and added comments to server code. Removed test clients from server folder.

--- server/auth.go
+++ server/auth.go
@@ -10,247 +10,225 @@ import (
   "encoding/json"
   "io/ioutil"
   "database/sql"
-
   _ "github.com/go-sql-driver/mysql"
-
   "golang.org/x/crypto/bcrypt"
-
   "github.com/gorilla/sessions"   
-
-  //"github.com/stretchr/objx"
-
 )
 
 
-// type authHandler struct {
-//   next http.Handler
-// }
-
-// func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-//   //if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie || cookie.value == "" {
-//   if cookie, err := r.Cookie("auth"); err == http.ErrNoCookie {
-//     fmt.Println(cookie)
-//     w.Header()["Location"] = []string{"/login"}
-//     w.WriteHeader(http.StatusTemporaryRedirect)
-//   } else if err != nil {
-//     panic(err.Error())
-//   } else {
-//     h.next.ServeHTTP(w, r)
-//   }
-// }
-
-// func MustAuth(handler http.Handler) http.Handler {
-//   return &authHandler{next: handler}
-// }
+//TODO: handle panics/errors, as unhandled panics/errors will shut down the server
 
 
+//function that adds a user to the database
+//TODO: fix all Writes to respond to the clients with the correct status
 func createUserHandler (w http.ResponseWriter, r *http.Request, db *sql.DB) {
-    fmt.Println("inside createUserHandler")
 
-    w.Header()["Access-Control-Allow-Origin"] = []string{"http://localhost:8080"}
-    w.Header()["access-control-allow-methods"] = []string{"GET, POST, OPTIONS"}
-    w.Header()["content-type"] = []string{"application/json"}
+  fmt.Println("Signing up user...")
 
-    if r.Method == "OPTIONS" {
-      fmt.Println("options request received")
-      w.WriteHeader(http.StatusTemporaryRedirect)
+  //add headers to response
+  w.Header()["Access-Control-Allow-Origin"] = []string{"http://localhost:8080"} //TODO: fix this?
+  w.Header()["access-control-allow-methods"] = []string{"GET, POST, OPTIONS"}
+  w.Header()["content-type"] = []string{"application/json"}
+
+  //ignore options requests
+  if r.Method == "OPTIONS" {
+    fmt.Println("options request received")
+    w.WriteHeader(http.StatusTemporaryRedirect)
+    return
+  }
+
+  //parse the body of the request into a string
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    panic(err)
+  }
+  //fmt.Println(string(body))
+  
+  //parse the JSON string body to get the username, password, firstname, and lastname
+  byt := body
+  var dat map[string]interface{}
+  if err := json.Unmarshal(byt, &dat); err != nil {
+    panic(err)
+  }
+  username := dat["username"].(string)
+  password := dat["password"].(string)
+  first := dat["firstname"].(string)
+  last := dat["lastname"].(string)
+
+  //if any of the above fields are blank, return an error saying so
+  for key, value := range dat {
+    if value == "" {
+      fmt.Println("about to write 400 header")
+      w.Write([]byte(fmt.Sprintf("Enter information for ", key)))
       return
     }
+  }
 
-    body, err := ioutil.ReadAll(r.Body)
+  //variable(s) to hold the returned values from the query
+  var (
+    queried_username string
+  )
+
+  //query the database for the username
+  err = db.QueryRow("select user_name from users where user_name = ?", username).Scan(&queried_username)
+  switch {
+
+  //if username doesn't exist
+  case err == sql.ErrNoRows:
+    //hash the password
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
     if err != nil {
-        panic(err)
+      fmt.Println(err)
     }
+    fmt.Println("hash is ", string(hash))
 
-    fmt.Println(string(body))
-    byt := body
-    var dat map[string]interface{}
-    if err := json.Unmarshal(byt, &dat); err != nil {
-      panic(err)
+    //add username, password, firstname, and lastname to database
+    stmt, err := db.Prepare("insert into users (user_name, first_name, last_name, password_hash) values (?, ?, ?, ?)")
+    if err != nil {
+      log.Fatal(err)
     }
-
-    username := dat["username"].(string)
-    password := dat["password"].(string)
-    first := dat["firstname"].(string)
-    last := dat["lastname"].(string)
-
-    // if any of the above fields are blank
-    // return error saying so
-    for key, value := range dat {
-      if value == "" {
-        fmt.Println("about to write 400 header")
-        w.Write([]byte(fmt.Sprintf("Enter information for ", key)))
-        return
-      }
+    res, err := stmt.Exec(username, first, last, string(hash))
+    if err != nil {
+      log.Fatal(err)
     }
-    // fmt.Println(w.Header())
+    lastId, err := res.LastInsertId()
+    if err != nil {
+      log.Fatal(err)
+    }
+    rowCnt, err := res.RowsAffected()
+    if err != nil {
+      log.Fatal(err)
+    }
+    fmt.Println("Inserted user " + username + " into database. Last inserted ID = %d, rows affected = %d\n", lastId, rowCnt)
 
-    var (
-      queried_username string
-    )
+    //create session for signed up player
+    session, err := store.Get(r, "flash-session")
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+    session.Values["id"] = lastId
+    session.Values["username"] = username   
+    session.AddFlash("This is a flashed message!", "message")
+    session.Save(r, w) 
+    
+    //return 200 status to indicate success
+    fmt.Println("about to write 200 header")
+    w.WriteHeader(http.StatusOK)
+    break
 
-    // query the database for the username
-    err = db.QueryRow("select user_name from users where user_name = ?", username).Scan(&queried_username)
-    switch {
-    // if username doesn't exist
-    case err == sql.ErrNoRows:
-        // hash password
-        hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
-        if err != nil {
-          fmt.Println(err)
-        }
-        fmt.Println("hash is ", string(hash))
-
-        // add username, password, first and last name to database
-        stmt, err := db.Prepare("insert into users (user_name, first_name, last_name, password_hash) values (?, ?, ?, ?)")
-        if err != nil {
-          log.Fatal(err)
-        }
-        res, err := stmt.Exec(username, first, last, string(hash))
-        if err != nil {
-          log.Fatal(err)
-        }
-        lastId, err := res.LastInsertId()
-        if err != nil {
-          log.Fatal(err)
-        }
-        rowCnt, err := res.RowsAffected()
-        if err != nil {
-          log.Fatal(err)
-        }
-        log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
-
-        // authCookieValue := objx.New(map[string]interface{}{
-        //   //what do I put here?
-        //   "userid":     lastId,
-        //   }).MustBase64()
-
-        // http.SetCookie(w, &http.Cookie{
-        //   Name:  "auth",
-        //   Value: authCookieValue,
-        //   // may want to change this route - insecure?
-        //   Path:  "/"})
-
-        // create session
-        session, err := store.Get(r, "flash-session")
-        if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        session.Values["id"] = lastId
-        session.Values["username"] = username   
-        session.AddFlash("This is a flashed message!", "message")
-        session.Save(r, w) 
-        
-        // redirect to connect (connect will open web socket)
-        //w.Header()["Location"] = []string{"/connect"}
-        fmt.Println("about to write 200 header")
-        w.WriteHeader(http.StatusOK)
-        break
-    case err != nil:
-        log.Fatal(err)
-        break
-    // if username exists
-    default:
-        fmt.Println("about to write 400 header")
-        w.Write([]byte(fmt.Sprintf("Username is already taken")))
-        break
-    } 
+  //if error querying database
+  case err != nil:
+    log.Fatal(err)
+    //return 400 status to indicate error
+    fmt.Println("about to write 400 header")
+    w.Write([]byte(fmt.Sprintf("Error querying database")))  
+    break
+  
+  //if username exists
+  default:
+    //return 400 status to indicate error
+    fmt.Println("about to write 400 header")
+    w.Write([]byte(fmt.Sprintf("Username is already taken")))
+    break
+  } 
 
 }
 
+//function that authenticates/signs in user
+//TODO: fix all Writes to respond to the clients with the correct status
+//TODO: handle logging in when a session is already assigned to client
 func loginHandler(w http.ResponseWriter, r *http.Request, db *sql.DB, store *sessions.CookieStore) {
 
-    fmt.Println("Authenticating User...")
+  fmt.Println("Authenticating User...")
 
-    w.Header()["access-control-allow-origin"] = []string{"http://localhost:8080"}                                                             
-    w.Header()["access-control-allow-methods"] = []string{"GET, POST, OPTIONS"}
-    w.Header()["Content-Type"] = []string{"application/json"}
+  //add headers to response
+  w.Header()["access-control-allow-origin"] = []string{"http://localhost:8080"} //TODO: fix this?                                                           
+  w.Header()["access-control-allow-methods"] = []string{"GET, POST, OPTIONS"}
+  w.Header()["Content-Type"] = []string{"application/json"}
 
-    if r.Method == "OPTIONS" {
-      fmt.Println("options request received")
-      w.WriteHeader(http.StatusTemporaryRedirect)
-      return
-    }
+  //ignore options requests
+  if r.Method == "OPTIONS" {
+    fmt.Println("options request received")
+    w.WriteHeader(http.StatusTemporaryRedirect)
+    return
+  }
 
-    body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        panic(err)
-    }
-    // fmt.Println(string(body))
+  //parse the body of the request into a string
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    panic(err)
+  }
+  //fmt.Println(string(body))
 
-    byt := body
-    var dat map[string]interface{}
-    if err := json.Unmarshal(byt, &dat); err != nil {
-      panic(err)
-    }
+  //parse the JSON string body to get the username and password
+  byt := body
+  var dat map[string]interface{}
+  if err := json.Unmarshal(byt, &dat); err != nil {
+    panic(err)
+  }
+  username := dat["username"].(string)
+  password := dat["password"].(string)
 
-    username := dat["username"].(string)
-    password := dat["password"].(string)
+  //variable(s) to hold the returned values from the query
+  var (
+    queried_id int
+    queried_password_hash string
+  )
 
-    fmt.Println(password)
+  //query the database for the username
+  err = db.QueryRow("select id, password_hash from users where user_name = ?", username).Scan(&queried_id, &queried_password_hash)
+  switch {
 
+  //if username doesn't exist   
+  case err == sql.ErrNoRows:
+    //return 400 status to indicate error
+    fmt.Println("about to write 400 header")
+    fmt.Println("Username cannot be found")     
+    w.Write([]byte(fmt.Sprintf("Username cannot be found"))) 
+    break
 
-    var (
-      queried_id int
-      queried_password_hash string
-    )
+  //if error querying database  
+  case err != nil:
+    log.Fatal(err)
+    //return 400 status to indicate error
+    fmt.Println("about to write 400 header")
+    w.Write([]byte(fmt.Sprintf("Error querying database")))  
 
-    err = db.QueryRow("select id, password_hash from users where user_name = ?", username).Scan(&queried_id, &queried_password_hash)
-    switch {
-    // if username doesn't exist   
-    case err == sql.ErrNoRows:
-      // send error back?
-      w.Header()["Location"] = []string{"/login"}
-      // w.WriteHeader(http.StatusTemporaryRedirect)
+  //if username exists  
+  default:
+    //fmt.Println("Retrieved Id is %d\n", queried_id)
+    //fmt.Println("Retrieved Password is %s\n", queried_password_hash)
+
+    //compare the retrieved password to the password sent by the client
+    err := bcrypt.CompareHashAndPassword([]byte(queried_password_hash), []byte(password))
+    if err != nil { 
+      //password not a match
+
+      //return 400 status to indicate error
       fmt.Println("about to write 400 header")
-      fmt.Println("Username cannot be found")     
-      w.Write([]byte(fmt.Sprintf("Username cannot be found"))) 
-      break
-    case err != nil:
-      log.Fatal(err)
-    default:
-      fmt.Printf("Retrieved Id is %d\n", queried_id)
-      fmt.Printf("Retrieved Password is %s\n", queried_password_hash)
+      fmt.Println("Password incorrect")     
+      w.Write([]byte(fmt.Sprintf("Password incorrect")))     
+    } else { 
+      //user is authorized
 
-      err := bcrypt.CompareHashAndPassword([]byte(queried_password_hash), []byte(password))
+      //create session
+      session, err := store.Get(r, "flash-session")
       if err != nil {
-          //password not a match
-          w.Header()["Location"] = []string{"/login"}
-          fmt.Println("about to write 400 header")
-          fmt.Println("Password incorrect")     
-          w.Write([]byte(fmt.Sprintf("Password incorrect")))     
-      } else {
-        // user is authorized
-
-        // authCookieValue := objx.New(map[string]interface{}{
-        //   //what do I put here?
-        //   "userid":     queried_id,
-        // }).MustBase64()
-
-        // http.SetCookie(w, &http.Cookie{
-        //   Name:  "auth",
-        //   Value: authCookieValue,
-        //   // may want to change this route - insecure?
-        //   Path:  "/"})
-
-        // create session
-        session, err := store.Get(r, "flash-session")
-        if err != nil {
-          http.Error(w, err.Error(), http.StatusInternalServerError)
-        }
-        session.Values["id"] = queried_id
-        session.Values["username"] = username
-        session.AddFlash("This is a flashed message!", "message")
-        session.Save(r, w)
-
-        w.Header()["Location"] = []string{"/connect"}
-        fmt.Println("about to write 200 header")
-        fmt.Println("password correct") 
-        w.WriteHeader(http.StatusOK)
+        http.Error(w, err.Error(), http.StatusInternalServerError)
       }
+      session.Values["id"] = queried_id
+      session.Values["username"] = username
+      session.AddFlash("This is a flashed message!", "message")
+      session.Save(r, w)
 
-      break
-
+      //return 200 status to indicate success
+      fmt.Println("about to write 200 header")
+      fmt.Println("password correct") 
+      w.WriteHeader(http.StatusOK)
     }
+
+    break
+  }
+
 }
 
--- server/server.go
+++ server/server.go
@@ -1,96 +1,106 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "flag"    
-    "log"
-    //"strings"
-    //"go/build"
-    //"path/filepath"
-    //"html/template"   
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql" 
-    "github.com/gorilla/sessions"   
+  "fmt"
+  "net/http"
+  "flag"    
+  "log"
+  //"strings"
+  //"go/build"
+  //"path/filepath"
+  //"html/template"   
+  "database/sql"
+  _ "github.com/go-sql-driver/mysql" 
+  "github.com/gorilla/sessions"   
 )
 
+//"constant" variables to be used throughout the program
 const (
-    DB_USER = "root"
-    DB_PASSWORD = ""
-    DB_NAME = "virtual_arm"
+  //database configuration information
+  DB_USER = "root"
+  DB_PASSWORD = ""
+  DB_NAME = "virtual_arm"
 )
 
+//variables to be used throughout the program
 var (
-    addr      = flag.String("addr", ":8080", "http service address")
+  //cookie information
+  store = sessions.NewCookieStore([]byte("a-secret-string"))
+
+  //server address information
+  addr = flag.String("addr", ":8080", "http service address")  
 )
-
-
-var store = sessions.NewCookieStore([]byte("a-secret-string"))
-
-
-func initializeDB() *sql.DB {
-    db, err := sql.Open("mysql",  DB_USER + ":" + DB_PASSWORD + "@/" + DB_NAME)
-    if err != nil {
-      panic(err)
-    } 
-
-    return db
-}
 
 func main() {
-    flag.Parse()
+  flag.Parse()
 
-    var db = initializeDB()
-    defer db.Close()
+  //open the database connection
+  var db = initializeDB()
+  defer db.Close() //defer closing the connection
 
-    //create the game room
-    var room = createGameRoom(1)
-    go room.run()
+  //create the game room
+  var room = createGameRoom(1)
+  go room.run()
 
-    //serve static assets
-    http.Handle("/", http.FileServer(http.Dir("../public")))
+  //serve static assets
+  http.Handle("/", http.FileServer(http.Dir("../public")))
 
-    //allow user to sign up
-    http.HandleFunc("/createUser", func(w http.ResponseWriter, r *http.Request) {
-        createUserHandler(w, r, db)
-    })    
+  //allow user to sign up
+  http.HandleFunc("/createUser", func(w http.ResponseWriter, r *http.Request) {
+    createUserHandler(w, r, db)
+  })    
 
-    //authenticate user
-    http.HandleFunc("/authenticate", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("hi")
-        loginHandler(w, r, db, store)
-    })
+  //authenticate user
+  http.HandleFunc("/authenticate", func(w http.ResponseWriter, r *http.Request) {
+    loginHandler(w, r, db, store)
+  })
 
-    //listen for player connection
-    http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println("trying to connect websocket")
-        connect(w, r, room, store)
-    })
+  //listen for player connection
+  http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("trying to connect websocket")
+    connect(w, r, room, store)
+  })
 
-    fmt.Println("Server starting")
-    err := http.ListenAndServe(*addr, nil)
-    if err != nil {
-        log.Fatal("ListenAndServe:", err)
-    }
+  //listen on specified port
+  fmt.Println("Server starting")
+  err := http.ListenAndServe(*addr, nil)
+  if err != nil {
+    log.Fatal("ListenAndServe:", err)
+  }
 }
 
+//function to open connection with database
+func initializeDB() *sql.DB {
+  db, err := sql.Open("mysql",  DB_USER + ":" + DB_PASSWORD + "@/" + DB_NAME)
+  if err != nil {
+    panic(err)
+  } 
+
+  return db
+}
+
+//handle the connect event which checks if the cookie corresponds to a logged in user
+//and creates the player in the game
+//TODO: Return correct status and message if session is invalid
 func connect(w http.ResponseWriter, r *http.Request, room *GameRoom, store *sessions.CookieStore) {
 
-    session, err := store.Get(r, "flash-session")
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-    fm := session.Flashes("message")
-    if fm == nil {
-      fmt.Println("Trying to log in as invalid user")
-      fmt.Fprint(w, "No flash messages")
-      return
-    }
-    session.Save(r, w)
+  //check for session to see if client is authenticated
+  session, err := store.Get(r, "flash-session")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+  fm := session.Flashes("message")
+  if fm == nil {
+    fmt.Println("Trying to log in as invalid user")
+    fmt.Fprint(w, "No flash messages")
+    return
+  }
+  session.Save(r, w)
 
-    fmt.Println("New user connected")
+  fmt.Println("New user connected")
 
-    playerHandler := PlayerHandler{Id: session.Values["id"].(int), Username: session.Values["username"].(string), Room: room}
+  //use the id and username attached to the session to create the player
+  playerHandler := PlayerHandler{Id: session.Values["id"].(int), Username: session.Values["username"].(string), Room: room}
 
-    playerHandler.createPlayer(w, r)
+  playerHandler.createPlayer(w, r)
 }
